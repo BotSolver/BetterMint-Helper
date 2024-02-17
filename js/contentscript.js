1ef36bf7e567e89387bdb2586d2b21a3382d9b40
BetterMInt <bettermint@basicsellout.tk>
2024-02-16 23:19:50 -0500

    Update contentscript.js

--- js/contentscript.js
+++ js/contentscript.js
@@ -8,28 +8,24 @@ function detectAndClick() {
   var nextGameButton = document.querySelector('button.ui_v5-button-component[aria-label="Next Game"]');
   var newGameButton = document.querySelector('button.ui_v5-button-component span.icon-font-chess.plus');
 
-  // Click rematch button if available
   if (rematchButton) {
     console.log('Rematch button found:', rematchButton);
     clickElement(rematchButton.closest('button'));
     console.log('Rematch button clicked.');
-    return; // Stop further processing
+    return;
   }
 
-  // Click accept rematch button if available
   if (acceptRematchButton) {
     console.log('Accept Rematch button found:', acceptRematchButton);
     clickElement(acceptRematchButton.closest('button'));
     console.log('Accept Rematch button clicked.');
-    return; // Stop further processing
+    return;
   }
 
-  // Click next game button if available
   if (nextGameButton) {
     console.log('Next Game button found:', nextGameButton);
     clickElement(nextGameButton);
     console.log('Next Game button clicked.');
-    // Delay before clicking the New Game button
     setTimeout(function() {
       if (newGameButton) {
         console.log('New Game button found:', newGameButton);
@@ -37,10 +33,9 @@ function detectAndClick() {
         console.log('New Game button clicked.');
       }
     }, 2000);
-    return; // Stop further processing
+    return;
   }
 
-  // Click New Game button directly if Next Game button not found
   if (newGameButton) {
     console.log('New Game button found:', newGameButton);
     clickElement(newGameButton.closest('button'));
@@ -50,5 +45,5 @@ function detectAndClick() {
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log('Content script loaded.');
-  setInterval(detectAndClick, 750); // Check every 3 seconds
+  setInterval(detectAndClick, 750);
 });
